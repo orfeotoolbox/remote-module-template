b51c33340715ae810746da5e5d8a7709aea25453
Manuel Grizonnet <manuel.grizonnet@gmail.com>
2015-05-07 13:34:32 +0200

    ENH: use OTB module macros

---
+++ otb-module.cmake
@@ -0,0 +1,19 @@
+set(DOCUMENTATION "OTB module template.")
+
+# itk_module() defines the module dependencies in ExternalTemplate
+# ExternalTemplate depends on ITKCommon
+# The testing module in ExternalTemplate depends on ITKTestKernel
+# and ITKMetaIO(besides ExternalTemplate and ITKCore)
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
+
+# define the dependencies of the include module and the tests
+otb_module(ExternalTemplate
+  DEPENDS
+    OTBCommon
+  TEST_DEPENDS
+    OTBTestKernel
+    OTBImageIO
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
