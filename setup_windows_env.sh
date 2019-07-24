c48041846aa73dbfd17a315476caa2c768b6c505
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-07-24 23:35:56 +0200

    CI: get more details on msvc install

--- setup_windows_env.sh
+++ setup_windows_env.sh
@@ -8,13 +8,13 @@ eval "$(python -c '
 import sys, os, subprocess
 import distutils.msvc9compiler as msvc
 msvc.find_vcvarsall=lambda _: sys.argv[1]
-envs=msvc.query_vcvarsall(sys.argv[2])
+envs=msvc.query_vcvarsall(arch='amd64')
 for k,v in envs.items():
     k = k.upper()
     v = ":".join(subprocess.check_output(["cygpath","-u",p]).decode("utf-8").rstrip(" \n") for p in v.split(";"))
     v = v.replace("'\''",r"'\'\\\'\''")
     print("export %(k)s='\''%(v)s'\''" % locals())
-' 'c:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/Tools/VsDevCmd.bat' '-arch=amd64'
+' 'c:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/Tools/VsDevCmd.bat'
 )"
 
 export CC=cl.exe
