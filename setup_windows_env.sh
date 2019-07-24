512e78187caf0d4e400b9c8a4dd5442260e2bceb
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-07-24 14:32:40 +0200

    CI: adaptation for Python 3

--- setup_windows_env.sh
+++ setup_windows_env.sh
@@ -7,8 +7,13 @@ msvc.find_vcvarsall=lambda _: sys.argv[1]
 envs=msvc.query_vcvarsall(sys.argv[2])
 for k,v in envs.items():
     k = k.upper()
-    v = ":".join(subprocess.check_output(["cygpath","-u",p]).rstrip() for p in v.split(";"))
+    v = ":".join(subprocess.check_output(["cygpath","-u",p]).decode("utf-8").rstrip(" \n") for p in v.split(";"))
     v = v.replace("'\''",r"'\'\\\'\''")
-    print "export %(k)s='\''%(v)s'\''" % locals()
+    print("export %(k)s='\''%(v)s'\''" % locals())
 ' 'c:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/Tools/VsDevCmd.bat' '-arch=amd64'
 )"
+
+export CC=cl.exe
+export CXX=cl.exe
+export CC_FOR_BUILD=cl.exe
+export CXX_FOR_BUILD=cl.exe
