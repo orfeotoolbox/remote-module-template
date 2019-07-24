9507144e56b55028a1e1c0210b26bb8e6667799b
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-07-25 01:01:28 +0200

    CI: use a proxy python script

--- setup_windows_env.sh
+++ setup_windows_env.sh
@@ -4,18 +4,7 @@ export PATH="/c/Python37:/c/ProgramData/chocolatey/lib/ninja/tools:$PATH"
 
 which python
 
-eval "$(python -c '
-import sys, os, subprocess
-import distutils.msvc9compiler as msvc
-msvc.find_vcvarsall=lambda _: sys.argv[1]
-envs=msvc.query_vcvarsall(arch="amd64")
-for k,v in envs.items():
-    k = k.upper()
-    v = ":".join(subprocess.check_output(["cygpath","-u",p]).decode("utf-8").rstrip(" \n") for p in v.split(";"))
-    v = v.replace("'\''",r"'\'\\\'\''")
-    print("export %(k)s='\''%(v)s'\''" % locals())
-' 'c:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/Tools/VsDevCmd.bat'
-)"
+eval "$(python vcvars_proxy.py 'c:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/Tools/VsDevCmd.bat' '-arch=amd64' '-host_arch=amd64' '-vcvars_ver=14.0')"
 
 export CC=cl.exe
 export CXX=cl.exe
