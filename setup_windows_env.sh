055c85b8381c7221f2b1b17a31f035f5767acbea
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-07-25 11:03:25 +0200

    CI: cleaning debug traces

--- setup_windows_env.sh
+++ setup_windows_env.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export PATH="/c/Python37:/c/ProgramData/chocolatey/lib/ninja/tools:$PATH"
-
-which python
-
 eval "$(python vcvars_proxy.py 'c:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/Tools/VsDevCmd.bat' '-arch=amd64' '-host_arch=amd64' '-vcvars_ver=14.0')"
 
 export CC=cl.exe
