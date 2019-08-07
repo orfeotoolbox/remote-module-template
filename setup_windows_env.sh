509913b8a840effc3ab02a1560dc7e7271febc64
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-08-07 23:18:30 +0200

    CI: remove unused file

--- setup_windows_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-eval "$(python vcvars_proxy.py 'c:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/Tools/VsDevCmd.bat' '-arch=amd64' '-host_arch=amd64' '-vcvars_ver=14.0')"
-
-export CC=cl.exe
-export CXX=cl.exe
-export CC_FOR_BUILD=cl.exe
-export CXX_FOR_BUILD=cl.exe
